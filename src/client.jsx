c62325ba3bda6e42d51b61402909106bcf00cdb1
mozahid <ce.muzahid@gmail.com>
2023-11-01 09:28:17 +0600

    added about feature

---
+++ src/client.jsx
@@ -0,0 +1,14 @@
+// import { SanityClient } from '@sanity/client'
+// import { ImageUrlBuilder } from '@sanity/image-url'
+
+// export const client = SanityClient({
+//   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+//   dataset: 'production',
+//   apiVersion: '2023-11-01',
+//   useCdn: true,
+//   token: process.env.REACT_APP_SANITY_TOKEN,
+// })
+
+// const builder = ImageUrlBuilder(client)
+
+// export const urlFor = (source) => builder.image(source)
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+// import { client, urlFor } from '../../client'
+import { images } from '../../constants'
 import './About.scss'
 
+const abouts = [
+  { title: ' Development', description: 'I am a Developer', imgUrl: '' },
+  { title: ' Development1', description: 'I am a Developer', imgUrl: '' },
+  { title: ' Development2', description: 'I am a Developer', imgUrl: '' }
+]
+
 const About = () => {
+  // const [abouts, setAbouts] = useState([])
+
   return (
-    <div>
-      About
-    </div>
+    <>
+      <h2 className="head-text">
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}>
+            <img src={images.about01} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
